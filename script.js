5c9f7d5612d50e7b994c49d7ce581585a164711a
estefaniaperezz <166597108+estefaniaperezz@users.noreply.github.com>
2024-07-03 12:32:06 +0200

    responsive web

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuIcon = document.getElementById('menu-icon');
-    const navbar = document.getElementById('navbar');
+    const navbar = document.querySelector('.navbar');
 
     menuIcon.addEventListener('click', function() {
         navbar.classList.toggle('active');
